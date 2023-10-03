9693650d97d150d23bd82822f799ea1bf735e3da
Potatochip147 <144011031+Potatochip147@users.noreply.github.com>
2023-10-03 16:14:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/1a2b6e76af675065491a7.jpg https://telegra.ph/file/f7a0e18e59b404d69ee3d.jpg https://telegra.ph/file/8e3de48f3db033b21f66a.jpg https://telegra.ph/file/8e3de48f3db033b21f66a.jpg')).split()
+PICS = (environ.get('PICS' ,'https://te.legra.ph/file/7f5895e02e532124fe20d.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
